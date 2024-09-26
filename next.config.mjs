76fe22b529b5800a18e9b6246b03b8250a0013ed
0xRVM <121198349+0xRVM@users.noreply.github.com>
2024-09-26 20:52:59 -0300

    Next Config Icons

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/primate-ico.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+      {
         source: '/primate-logo.png',
         headers: [
           {
