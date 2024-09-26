b4c61ef3ca0383a193d91fb01e8d7fb7a0d05ffd
0xRVM <121198349+0xRVM@users.noreply.github.com>
2024-09-25 22:39:48 -0300

    Back to TxT version

--- app/page.tsx
+++ app/page.tsx
@@ -50,25 +50,22 @@ export default function PrimateClosureAnnouncement() {
             </ul>
           </section>
           
-<section className="text-center mt-24">
-  <h2 className="text-2xl font-semibold mb-4 text-center">Gracias</h2>
-  <p className="text-gray-300 leading-normal">
-    A nuestros jugadores, staff, patrocinadores y, lo más importante, a nuestros fans: gracias por su inquebrantable apoyo
-    a lo largo de este increíble viaje. Ustedes fueron desde el primer momento la fuerza impulsora detrás de nuestro éxito, y estamos eternamente agradecidos.
-  </p>
-</section>
+          <section>
+            <h2 className="text-2xl font-semibold mb-4 text-center">Gracias</h2>
+            <p className="text-gray-300 leading-normal">
+              A nuestros jugadores, staff, patrocinadores y, lo más importante, a nuestros fans: gracias por su inquebrantable apoyo
+              a lo largo de este increíble viaje. Ustedes fueron desde el primer momento la fuerza impulsora detrás de nuestro éxito, y estamos eternamente agradecidos.
+            </p>
+          </section>
 
-<section className="text-center mt-24">
-  <h2 className="text-2xl font-semibold mb-4">El Legado Continúa</h2>
-  <p className="text-gray-300 mb-6 leading-relaxed">
-    Aunque Primate cierre sus puertas, su espíritu seguirá latiendo en las vidas que tocamos y en la marca imborrable que dejamos en los Esports.
-    Nuestro legado persiste, esperando su momento para despertar nuevamente en un futuro aún por escribir. 
-  </p>
-</section>
-
-<section className="text-center mt-24 mb-24">
-  <p className="text-3xl font-bold animate-rainbow-text">#NuncaTeRindas</p>
-      </section>
+          <section className="text-center">
+            <h2 className="text-2xl font-semibold mb-4">El Legado Continúa</h2>
+            <p className="text-gray-300 mb-6 leading-relaxed">
+              Aunque Primate cierre sus puertas, su espíritu seguirá latiendo en las vidas que tocamos y en la marca imborrable que dejamos en los Esports.
+              Nuestro legado persiste, esperando su momento para despertar nuevamente en un futuro aún por escribir. 
+            </p>
+            <p className="text-3xl font-bold animate-rainbow-text">#NuncaTeRindas</p>
+          </section>
 </article>
         
 <footer className="mt-24 text-center">
